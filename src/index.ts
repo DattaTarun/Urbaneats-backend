325787360044f759d60cd91faa165bcf7210108d
Tarun Datta <tarundatta@Taruns-MacBook-Air.local>
2024-06-19 22:17:54 +0530

    adding user profile feature

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import express, { Request,Response } from "express";
 import cors from "cors";
 import "dotenv/config"
 import mongoose from "mongoose";
-import MyUserRoute from "./routes/MyUserRoute"
+import myUserRoute from "./routes/MyUserRoute"
+import {v2 as cloudinary} from "cloudinary"
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
 
 const app= express();
 app.use(express.json())
@@ -10,11 +12,18 @@ app.use(cors())
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=>console.log("connected to database"));
 
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET,
+})
+
 app.get("/health",async(req:Request,res:Response)=>{
     res.send({message:"Health OK!"})
 })
 
-app.use("/api/my/user",MyUserRoute);
+app.use("/api/my/user",myUserRoute);
+app.use("/api/my/restaurant",myRestaurantRoute);
 
 app.listen(2000,()=>{
     console.log("server started on port 2000")
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,28 +1,27 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const userSchema=new mongoose.Schema({
-    auth0Id:{
-        type:String,
-        required: true,
-    },
-    email:{
-        type:String,
-        required:true
-    },
-    name:{
-        type:String,
-    },
-    addressLine1:{
-        type:String
-    },
-    city:{
-        type:String
-    },
-    country:{
-        type:String
-    },
-   
-})
+const userSchema = new mongoose.Schema({
+  auth0Id: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  addressLine1: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+});
 
-const user=mongoose.model("User",userSchema);
-export default user;
+const User = mongoose.model("User", userSchema);
+export default User;
